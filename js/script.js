a37d12e2de12e572f30ca203f763916da79f300c
ChiaraS97 <chiara.sucato97@gmail.com>
2023-03-14 14:55:53 +0100

    remove static markup, create variables and for loop

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// Consegna:
+// Dato un array contenente una lista di cinque immagini, creare un carosello.
+
+//creo array con 5 immagini all'interno
+
 const images = [
     'img/01.webp',
     'img/02.webp',
@@ -5,3 +10,24 @@ const images = [
     'img/04.webp',
     'img/05.webp'
 ]
+
+// creo una variabile per prendere lo slider dove andrò ad inserire le 5 immagini
+
+const slider = document.querySelector('.slider');
+
+// creo una variabile vuota dove andrò ad inserire un template literal
+let slides = '';
+
+// creo un ciclo for in cui vi sarà la variabile slides con il suo template literal 
+
+for (let i = 0; i < images.length; i++) {
+    slides = `
+    <div class="slide">
+    <img src="${images}" alt="character-${i}" >
+    </div>
+    `
+}
+
+// aggiungo alla variabile slider il contenuto della variabile slides
+
+slider.innerHTML += slides;

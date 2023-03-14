fa2afc576ec819a567780c3952bb0de982162d1d
ChiaraS97 <chiara.sucato97@gmail.com>
2023-03-14 17:40:08 +0100

    add event on button.previous

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ let slides = '';
 // creo un ciclo for in cui vi sarà la variabile slides con il suo template literal 
 
 for (let i = 0; i < images.length; i++) {
-    slides = `
+    slides += `
     <div class="slide">
     <img src="${images[i]}" alt="character-${i}" >
     </div>
@@ -39,7 +39,9 @@ slider.innerHTML += slides;
 // - creo una variabile "currentIndex" con dentro l'indice dell'imagine che voglio mostrare per prima 
 // - aggiungo alla slide con quell'index una classe active (con d-block), per renderla visibile
 let currentIndex = 0;
-document.querySelectorAll('.slide')[currentIndex].classList.add('active');
+document.querySelectorAll(".slide")[currentIndex].classList.add('active');
+
+
 
 // creo due variabili per prendere i bottoni su cui metterò un evento al click
 
@@ -50,17 +52,34 @@ const next = document.querySelector('.next');
 
 next.addEventListener('click', goNext);
 
-// creo la funzione "goNext" indicando le istruzione per passare da una slide ad un'altra, rimuovendo la classe "active".
+// creo la funzione "goNext" indicando le istruzioni per passare da una slide ad un'altra, rimuovendo la classe "active".
 
 function goNext() {
-    document.querySelectorAll('.slide')[currentIndex].classList.remove('active');
+    document.querySelectorAll(".slide")[currentIndex].classList.remove('active');
     // condizione per ripartire dalla prima slide dopo essere arrivati all'ultima
-    if (currentIndex === images.lenght -1) {
+    if (currentIndex === images.length -1) {
         currentIndex = 0;
     } else {
         currentIndex++;
+        
     }
 
     // a questo punto l'immagine con il valore di currentIndex attuale prenderà la classe "active" e sarà visibile
+    document.querySelectorAll(".slide")[currentIndex].classList.add('active');
+}
+
+
+// creo la funzione "goPrev" indicando le istruzioni per andare alla slide precedente, rimuovendo la classe "active".
+prev.addEventListener('click', goPrev);
+
+function goPrev() {
+    document.querySelectorAll('.slide')[currentIndex].classList.remove('active');
+    if (currentIndex === 0) {
+        currentIndex = images.length -1;
+    } else {
+        currentIndex--;
+    }
+   
     document.querySelectorAll('.slide')[currentIndex].classList.add('active');
+
 }
